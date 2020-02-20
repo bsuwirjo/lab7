dd8e768bcf8ecc7745e782ecf3eae7b79944d715
Bernard <bsuwirjo@gmail.com>
2020-02-20 09:13:53 -0800

    Added google analytics functionality

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ var projects = require('../projects.json');
 
 exports.view = function(request, response){
   	response.render('index', projects);
+    projects['viewAlt'] = false;
+};
+
+exports.viewAlt = function(request, response){
+	response.render('index', projects);
+	projects['viewAlt'] = true;
+};
+
+function likeButton() {
+	ga("send", "event", 'like', 'click');
 };
